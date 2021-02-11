0a9dc3b2d95c930dd38b9fd121827e01196b8f1c
Jun <junterashima0817@gmail.com>
2021-02-12 07:36:12 +0900

    Features added

--- Pages/Teams/Team.cshtml.cs
+++ Pages/Teams/Team.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using League.Data;
 using League.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace League.Pages.Teams
 {
@@ -15,13 +16,17 @@ namespace League.Pages.Teams
         [BindProperty(SupportsGet =true)]
         public string TeamId { get; set; }
         public Team Team { get; set; }
+        public Conference Conference { get; set; }
         public TeamModel(LeagueContext context)
         {
             _context = context;
         }
         public async Task OnGetAsync()
         {
-            Team = await _context.Teams.FindAsync(TeamId);
+            Team = await _context.Teams
+                .Include(t => t.Players)
+                .Include(t => t.Division)
+                .FirstOrDefaultAsync(t => t.TeamId == TeamId);
         }
     }
 }
