dfc5dec605cc2660e93541a90fa1bfc8b9fb112b
Jun <junterashima0817@gmail.com>
2021-02-15 08:39:50 +0900

    search buttons added

--- Pages/Players/Index.cshtml.cs
+++ Pages/Players/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using League.Data;
 using League.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace League.Pages.Players
 {
@@ -15,7 +16,17 @@ namespace League.Pages.Players
         private readonly LeagueContext _context;
         public List<Player> Players { get; set; }
         public List<Team> Teams { get; set; }
+        public SelectList Positions { get; set; }
         public string TeamName { get; set; }
+
+        [BindProperty(SupportsGet =true)]
+        public string SortString { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchTeam { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchPosition { get; set; }
 
         public IndexModel(LeagueContext context)
         {
@@ -31,6 +42,40 @@ namespace League.Pages.Players
             var players = from player in _context.Players
                           select player;
             players = players.OrderBy(player => player.Name);
+
+            if (!String.IsNullOrEmpty(SortString))
+            {
+                switch (SortString)
+                {
+                    case "Number":
+                        players = players.OrderBy(player => player.Number);
+                        break;
+                    case "Name":
+                        players = players.OrderBy(player => player.Name);
+                        break;
+                    case "Position":
+                        players = players.OrderBy(player => player.Position);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            var positionQuery = from player in _context.Players
+                                select player.Position;
+            Positions = new SelectList(await positionQuery.Distinct().ToListAsync());
+            
+            if(!String.IsNullOrEmpty(SearchTeam))
+            {
+                players = from player in players
+                          where player.TeamId == SearchTeam
+                          select player;
+            }
+            if(!String.IsNullOrEmpty(SearchPosition))
+            {
+                players = from player in players
+                          where player.Position == SearchPosition
+                          select player;
+            }
 
             Players = players.ToList();
         }
